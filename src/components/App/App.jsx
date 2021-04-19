ae47c691ad4e71f1da7376f2cf4061e500b16c93
Emre <bQnote197@gmail.com>
2021-04-19 13:26:10 +0300

    1 git commit -m 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-// import { Main } from "../Main/Main.jsx";
+import { Main } from "../Main/Main.jsx";
 
 import "./App.css";
 
-export function App() {
+export const App = () => {
   return (
-
-      <header></header>,
-      <main>
-        {[[1], [2], [3]].map((item) => (
-          <ul>
-            <li>{item}</li>
-          </ul>
-        ))}
-      </main>
-      ,<footer></footer>,
-
+    <div>
+      <Main />
+    </div>
   );
-}
+};
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,1 +1,5 @@
+import React from "react";
 
+export const Main = () => {
+  return <div>a</div>;
+};
